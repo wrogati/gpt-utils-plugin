3f56591061a178de1e42ed4d573eecf3ef45d73f
Wellington Rogati <wellingtonrogati@gmail.com>
2024-08-11 21:37:09 -0300

    Implemented error handler and miss configuration

--- gtp.service.ts
+++ gtp.service.ts
@@ -1,29 +1,67 @@
-import { GPTSettings } from "main";
+import { GPTSettings, TranslationOptionsSettings } from "main";
 import OpenAI from "openai";
 
 export class GPTService {
 	private openai: OpenAI;
+	private translateSettings: TranslationOptionsSettings;
 
-	constructor(settings: GPTSettings) {
+	constructor(
+		settings: GPTSettings,
+		translateSettings: TranslationOptionsSettings
+	) {
+		if (settings.openaiApiKey === "empty") {
+			throw new OpenAIAPIKeyMisConfigurationException();
+		}
+
 		this.openai = new OpenAI({
 			apiKey: settings.openaiApiKey,
 			dangerouslyAllowBrowser: true,
 		});
+
+		this.translateSettings = translateSettings;
 	}
 
 	async textTranslate(textContent: string): Promise<string> {
-		const completion = await this.openai.chat.completions.create({
-			messages: [
-				{ role: "system", content: "You are a translator." },
-				{
-					role: "user",
-					content: `translate the text below to english: ${textContent}`,
-				},
-			],
-			model: "gpt-4o",
-		});
+		if (this.translateSettings.targetLang.isoCode === "empty") {
+			throw new TargetLangMisConfigurationException();
+		}
 
-		//todo create a error exception for try/catch
-		return completion.choices[0].message.content || "";
+		try {
+			let userContent = `translate the text below to ${this.translateSettings.targetLang.label} (${this.translateSettings.targetLang.isoCode}): \n${textContent}`;
+
+			const completion = await this.openai.chat.completions.create({
+				messages: [
+					{ role: "system", content: "You are a translator." },
+					{
+						role: "user",
+						content: userContent,
+					},
+				],
+				model: "gpt-4o",
+			});
+
+			return completion.choices[0].message.content || "";
+		} catch (error) {
+			if (error instanceof Error) {
+				if (error.message.includes("Incorrect API key provided")) {
+					throw new OpenAIAPIKeyMisConfigurationException(
+						error.stack
+					);
+				}
+			}
+			throw error;
+		}
+	}
+}
+
+export class TargetLangMisConfigurationException extends Error {
+	constructor() {
+		super("Target language is not defined");
+	}
+}
+
+export class OpenAIAPIKeyMisConfigurationException extends Error {
+	constructor(stack?: string) {
+		super("OpenAI API Key is not defined or is invalid");
 	}
 }
